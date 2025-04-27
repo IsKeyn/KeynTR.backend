4b3141b3e1af4489c6c82ddc9ea86b34ca1d3269
Keyn <keyn-artur@yandex.ru>
2025-04-27 16:24:22 +0500

    Локализация для письма en

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'account_verification_theme' => 'Подтверждение учетной записи на сайте ' . config('publicApp.public_url'),
+    'account_verification' => 'Мы отправили вам email с ссылкой для подтверждения',
+    'reset_password_subject' => 'Сброс пароля на сайте ' . config('publicApp.public_url'),
+];
