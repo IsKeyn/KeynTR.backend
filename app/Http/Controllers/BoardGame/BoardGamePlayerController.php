999606714e441b89c005ff5ed8c07f9d6bd739c5
Keyn <keyn-artur@yandex.ru>
2025-04-30 21:00:33 +0500

    - Больше информации об игроке - Ресурс для BoardGamePosition

--- app/Http/Controllers/BoardGame/BoardGamePlayerController.php
+++ app/Http/Controllers/BoardGame/BoardGamePlayerController.php
@@ -4,22 +4,37 @@ namespace App\Http\Controllers\BoardGame;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BoardGame\BoardGameInventoryResource;
+use App\Http\Resources\BoardGame\BoardGamePlayerPositionsResource;
 use App\Http\Resources\BoardGame\BoardGamePlayerResource;
+use App\Http\Resources\BoardGame\LogResource;
 use App\Models\BoardGame\BoardGameInventory;
+use App\Models\BoardGame\BoardGameLog;
 use App\Models\BoardGame\BoardGamePlayer;
+use App\Models\BoardGame\BoardGamePlayerPosition;
 use Illuminate\Http\Request;
 
 class BoardGamePlayerController extends Controller
 {
-    public function get($id, Request $request, BoardGamePlayer $BoardGamePlayer, BoardGameInventory $BoardGameInventory)
+    public function get(
+        $id,
+        Request $request,
+        BoardGamePlayer $BoardGamePlayer,
+        BoardGameInventory $BoardGameInventory,
+        BoardGameLog $BoardGameLog,
+        BoardGamePlayerPosition $BoardGamePlayerPosition
+    )
     {
         $player = $BoardGamePlayer->where('user_id', $id)->where('board_game_id', $request->board_game_id)->first();
 //        $inventory = $BoardGamePlayer->inventory->where('board_game_id', $request->board_game_id);
         $inventory = $BoardGameInventory->where('user_id', $id)->where('board_game_id', $request->board_game_id)->get();
+        $logs = $BoardGameLog->where('created_by', $id)->where('board_game_id', $request->board_game_id)->orderByDesc('id')->limit(100)->get();
+        $steps = $BoardGamePlayerPosition->where('user_id', $id)->where('board_game_id', $request->board_game_id)->orderByDesc('id')->limit(100)->get();
 
         return [
             'player_info' => BoardGamePlayerResource::make($player),
             'inventory' => BoardGameInventoryResource::collection($inventory),
+            'logs' => LogResource::collection($logs),
+            'steps' => BoardGamePlayerPositionsResource::collection($steps),
         ];
     }
 
---
+++ app/Http/Resources/BoardGame/BoardGamePlayerPositionsResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\BoardGame;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BoardGamePlayerPositionsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'position' => $this->position,
+            'board_game_id' => $this->board_game_id,
+            'created_by' => $this->created_by,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
