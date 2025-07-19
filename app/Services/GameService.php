6c4bac6308356f7738cd92f202425c067d01b815
Keyn <keyn-artur@yandex.ru>
2025-07-19 21:25:02 +0500

    - Фикс ошибки с пустой датой

--- app/Services/GameService.php
+++ app/Services/GameService.php
@@ -17,34 +17,37 @@ class GameService extends ServiceProvider
             $arGamingPlatformsIds = [];
 
             foreach ($releaseDates as $item) {
-                // Ищем дату, которая равна переданой и привязана к текущеё сущности
-                $dateQuery = Date::where('date', $item['date']);
-                $dateQuery->whereHas('games', function ($q) use ($entity) {
-                    $q->where('games.id', $entity->id);
-                });
-
-                $gamePlatformEntity = GamingPlatform::where('id', $item['gaming_platform'])->first();
-
-                if ($gamePlatformEntity && $gamePlatformName = $gamePlatformEntity->name) {
-                    $dateQuery->whereHas('gamePlatform', function ($q) use ($gamePlatformEntity) {
-                        $q->where('gaming_platforms.id', $gamePlatformEntity->id);
+                if (isset($item['date']) && $item['date']) {
+                    /* Ищем дату, которая равна переданой и привязана к текущеё сущности */
+                    $dateQuery = Date::where('date', $item['date']);
+                    $dateQuery->whereHas('games', function ($q) use ($entity) {
+                        $q->where('games.id', $entity->id);
                     });
 
-                    $dateEntity = $dateQuery->first();
-                } else {
-                    return false;
-                }
+                    $gamePlatformEntity = GamingPlatform::where('id', $item['gaming_platform'])->first();
 
-                if (!$dateEntity) {
-                    $dateEntity = Date::create(['date' => $item['date']]);
-                }
+                    if ($gamePlatformEntity && $gamePlatformName = $gamePlatformEntity->name) {
+                        $dateQuery->whereHas('gamePlatform', function ($q) use ($gamePlatformEntity) {
+                            $q->where('gaming_platforms.id', $gamePlatformEntity->id);
+                        });
 
-                $arDatesIds[$dateEntity->id] = [];
+                        $dateEntity = $dateQuery->first();
+                    } else {
+                        return false;
+                    }
 
-                if ($gamePlatformEntity) {
-                    if ($dateEntity) {
-                        $dateEntity->gamePlatform()->syncWithPivotValues($gamePlatformEntity->id, ['additional_info' => $item['addInfo']]);
-                        $arGamingPlatformsIds[$gamePlatformEntity->id] = [];
+                    if (!$dateEntity) {
+                        $dateEntity = Date::create(['date' => $item['date']]);
+                    }
+
+                    $arDatesIds[$dateEntity->id] = [];
+
+                    if ($gamePlatformEntity) {
+                        if ($dateEntity) {
+                            $dateEntity->gamePlatform()->syncWithPivotValues($gamePlatformEntity->id,
+                                ['additional_info' => $item['addInfo']]);
+                            $arGamingPlatformsIds[$gamePlatformEntity->id] = [];
+                        }
                     }
                 }
             }
@@ -60,22 +63,24 @@ class GameService extends ServiceProvider
             $arDatesIds = [];
 
             foreach ($anonsDates as $item) {
-                /* Ищем дату, которая равна переданой и привязана к текущеё сущности с типом DATE_ANONS_TYPE */
-                $dateQuery = Date::where('date', $item['date']);
-                $dateQuery->whereHas(
-                    'gamesAnons',
-                    function ($q) use ($entity) {
-                        $q->where('games.id', $entity->id);
+                if (isset($item['date']) && $item['date']) {
+                    /* Ищем дату, которая равна переданой и привязана к текущеё сущности с типом DATE_ANONS_TYPE */
+                    $dateQuery = Date::where('date', $item['date']);
+                    $dateQuery->whereHas(
+                        'gamesAnons',
+                        function ($q) use ($entity) {
+                            $q->where('games.id', $entity->id);
+                        }
+                    );
+
+                    $dateEntity = $dateQuery->first();
+
+                    if (!$dateEntity) {
+                        $dateEntity = Date::create(['date' => $item['date']]);
                     }
-                );
 
-                $dateEntity = $dateQuery->first();
-
-                if (!$dateEntity) {
-                    $dateEntity = Date::create(['date' => $item['date']]);
+                    $arDatesIds[] = $dateEntity->id;
                 }
-
-                $arDatesIds[] = $dateEntity->id;
             }
 
             // TODO не работает выяснить причину
