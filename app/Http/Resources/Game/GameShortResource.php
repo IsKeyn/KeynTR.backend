eac74198d19c2d14b35da6708e21016e9337c3cd
Keyn <keyn-artur@yandex.ru>
2025-09-09 15:40:05 +0500

    - Ресурсы игры

--- app/Http/Resources/Game/GameShortResource.php
+++ app/Http/Resources/Game/GameShortResource.php
@@ -1,15 +1,22 @@
 <?php
 
-namespace App\Http\Resources;
+namespace App\Http\Resources\Game;
 
+use App\Http\Resources\AnonsDateResource;
+use App\Http\Resources\CompanyResource;
+use App\Http\Resources\GenreResource;
 use App\Http\Resources\Admin\LinkResource;
+use App\Http\Resources\GroupResource;
+use App\Http\Resources\MediaResource;
+use App\Http\Resources\ReleaseDateResource;
+use App\Http\Resources\TagResource;
 use App\Models\Game;
 use App\Models\Media;
 use App\Models\VotesLog;
 use App\Services\VotesService;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class GameResource extends JsonResource
+class GameShortResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -41,13 +48,9 @@ class GameResource extends JsonResource
             'companies' => CompanyResource::collection($this->company),
             'links' => LinkResource::collection($this->link),
             'additional_fields' => $this->additionalFields,
-            'seo' => $this->seo && $this->seo->count() ? SeoResource::make($this->seo) : null,
             'views' => $this->views ? $this->views->value : null,
             'likes' => $this->likes ? $this->likes->value : null,
-            'menu' => MenuTypeResource::collection($this->menu),
-            'blocks' => BlockResource::collection($this->blocks),
             'already_voted' => VotesService::alreadyVoted($this->model, $this->id, VotesLog::LIKE, $request->user() ? $request->user()->id : null),
-            'comments_count' => $this->comments->count(),
             'created_by' => $this->created_by,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
