15a0d2f11419627e2eac505c512f01ebf28987e4
Keyn <keyn-artur@yandex.ru>
2025-09-12 02:25:45 +0500

    - ItemBindResource, ресурс для привязки предметов к настольной игре

---
+++ app/Http/Resources/BoardGame/ItemBindResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\BoardGame;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ItemBindResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'board_game_id' => $this->board_game_id,
+            'active' => $this->active,
+            'item' => ItemResource::make($this->item),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
