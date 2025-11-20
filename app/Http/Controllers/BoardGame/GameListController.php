8cb71b228eee8c4d2b145d112582c4f39001a1d2
Keyn <keyn-artur@yandex.ru>
2025-11-21 01:38:28 +0500

    Кеш списка игр

--- app/Http/Controllers/BoardGame/GameListController.php
+++ app/Http/Controllers/BoardGame/GameListController.php
@@ -7,15 +7,21 @@ use App\Http\Resources\BoardGame\GameListResource;
 use App\Models\BoardGame\BoardGame;
 use App\Models\BoardGame\BoardGameGameList;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class GameListController extends Controller
 {
     public function list(Request $request, BoardGameGameList $BoardGameGameList)
     {
-        $boardGame = BoardGame::findBySlug($request->slug)->active()->first();
+        $cacheKey = 'bg_game_list_cache_' . $request->slug;
+        $minutes = 1440; // 24 часа в минутах
 
-        if ($boardGame) {
-            return GameListResource::collection($BoardGameGameList::query()->findByBoardGame($boardGame->id)->active()->get());
-        }
+        return Cache::remember($cacheKey, $minutes, function () use ($request, $BoardGameGameList) {
+            $boardGame = BoardGame::findBySlug($request->slug)->active()->first();
+
+            if ($boardGame) {
+                return GameListResource::collection($BoardGameGameList::query()->findByBoardGame($boardGame->id)->active()->get());
+            }
+        });
     }
 }
