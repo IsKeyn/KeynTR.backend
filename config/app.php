afaf390a03f4978998b6a0a24af5527e6df35ae9
Keyn <keyn-artur@yandex.ru>
2026-05-12 15:13:57 +0500

    Переход на версию 10

--- config/app.php
+++ config/app.php
@@ -179,6 +179,8 @@ return [
 
 //        Laravel\Socialite\SocialiteServiceProvider::class,
         \SocialiteProviders\Manager\ServiceProvider::class,
+
+        Laravel\Telescope\TelescopeServiceProvider::class,
     ],
 
     /*
