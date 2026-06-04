4b23fd852e986ab03d556f16e627ac3d8b938303
Keyn <keyn-artur@yandex.ru>
2026-06-04 15:52:14 +0500

    - Фикс cors ошибки

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('PA_PUBLIC_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
