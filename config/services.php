9f3f3a5602dc788252fd851897caf621d85dfc59
Keyn <keyn-artur@yandex.ru>
2026-07-27 19:17:09 +0500

    - Рефакторинг oauth

--- config/services.php
+++ config/services.php
@@ -47,4 +47,10 @@ return [
         'client_secret' => env('VK_CLIENT_SECRET'),
         'redirect' => env('VK_REDIRECT_URI'),
     ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 ];
